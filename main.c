6c07c858bcf3e373d5197ee576a07b2ad00269bc
Antonis Karafotis <antoniskarafotis2@gmail.com>
2026-01-01 13:51:38 +0200

    Refactor main.c for better readability and performance
    Refactor code for clarity and performance improvements.

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include"raylib.h"
+#include <time.h>
 
+#include"helpers.c"
 #include"drawTextures.c"
 
-const int INITIAL_WINDOW_WIDTH = 1000;
+const int INITIAL_WINDOW_WIDTH = 1000; 
 const int INITIAL_WINDOW_HEIGHT = 550;
 const int DELIVERY_BIKE_RENDER_SIZE = 32;
 const int DELIVERY_BIKE_SCALED_SIZE = 128;
 const Color BACKGROUND_COLOR = DARKGRAY;
-bool showOrders=false;
-int count=0; //Number of order (Αριθμός παραγγελίας)
 
+const int MINIMAP_WIDTH = 150;      
+const int MINIMAP_HEIGHT = 150;     
+const float MINIMAP_ZOOM = 0.3f;    
+const int MINIMAP_BORDER = 2;
+
+bool showOrders=false;
+int count=0; //Number of order
 int main(void) {
   
+  SetRandomSeed(time(NULL)); 
+  Vehicle vehicles[MAX_VEHICLES];
+  vehicleGenerator(MAX_VEHICLES, vehicles, 80, 80); // posx , posy != 0 else bug. Needs to be offsected
+  
   InitWindow(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT, "RaylibProjectAuth");
-  Texture2D Texture2DBackground = LoadTexture("map.jpg"); 
+  Texture2D background = LoadTexture("map.jpg"); 
+  SetTextureFilter(background, TEXTURE_FILTER_POINT);
+  
+  int mapHeight = background.height;
+  int mapWidth = background.width;
+  
   SetTargetFPS(60);
 
   RenderTexture2D deliveryBikeRender = LoadRenderTexture(DELIVERY_BIKE_RENDER_SIZE, DELIVERY_BIKE_RENDER_SIZE);
@@ -24,15 +40,21 @@ int main(void) {
     0, 0, deliveryBikeRender.texture.width, deliveryBikeRender.texture.height
   };
   
-  Rectangle deliveryBike = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f, DELIVERY_BIKE_SCALED_SIZE, DELIVERY_BIKE_SCALED_SIZE};
+  Rectangle deliveryBike = {mapWidth / 2.0f, mapHeight / 2.0f, DELIVERY_BIKE_SCALED_SIZE, DELIVERY_BIKE_SCALED_SIZE};
   
-  // initialize camera
+  // initialize main camera
   Camera2D cam = {0};
-  cam.offset = (Vector2){GetScreenWidth() / 2.0f,GetScreenHeight() / 2.0f};
+  cam.offset = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
   cam.zoom = 1;
   cam.rotation = 0;
+  
+  // initialize minimap camera
+  Camera2D minimapCam = {0};
+  minimapCam.zoom = MINIMAP_ZOOM;
 
   while (!WindowShouldClose()) {
+    
+    updateTraffic(vehicles, MAX_VEHICLES, cam, mapHeight, mapWidth);
     BeginDrawing();
 
       ClearBackground(DARKGRAY);
@@ -40,19 +62,19 @@ int main(void) {
       /*** MOVEMENT ***/ 
       // move forward
       if (IsKeyDown(KEY_W)) {
-        deliveryBike.y -= 2;
+        deliveryBike.y -= 4;
       }
       // move backward (S)
       if (IsKeyDown(KEY_S)) {
-        deliveryBike.y += 2;
+        deliveryBike.y += 4;
       }
       // move left (A)
       if(IsKeyDown(KEY_A)) {
-        deliveryBike.x -= 2;
+        deliveryBike.x -= 4;
       }
       // move right (D)
       if (IsKeyDown(KEY_D)) {
-        deliveryBike.x += 2;
+        deliveryBike.x += 4;
       }
 
       /*** CAMERA ***/
@@ -68,13 +90,52 @@ int main(void) {
         cam.zoom += 0.2;
       }
       
+      minimapCam.target.x = deliveryBike.x;
+      minimapCam.target.y = deliveryBike.y;
+      
       BeginMode2D(cam);
         
-        DrawTexture (Texture2DBackground, 0, 0, WHITE);
+        DrawTexture(background, 0, 0, WHITE);
+        for (int i=0; i<MAX_VEHICLES; i++)  {
+            mapVehicleToDrawFunction(vehicles[i].type, vehicles[i].posx, vehicles[i].posy, vehicles[i].vehicleColor);
+        }
+        
         Draw8BitRoad();
+        
+        Rectangle destRect = { deliveryBike.x, deliveryBike.y, DELIVERY_BIKE_SCALED_SIZE, DELIVERY_BIKE_SCALED_SIZE };
+        Vector2 origin = { DELIVERY_BIKE_SCALED_SIZE / 2, DELIVERY_BIKE_SCALED_SIZE / 2 };
         DrawTexturePro(deliveryBikeRender.texture, bikeSource, deliveryBike, (Vector2) { DELIVERY_BIKE_SCALED_SIZE / 2, DELIVERY_BIKE_SCALED_SIZE / 2 }, 180, WHITE);
     
       EndMode2D();
+      
+      int mmX = GetScreenWidth() - MINIMAP_WIDTH - 20;
+      int mmY = 20;
+      
+      minimapCam.offset = (Vector2){ mmX + MINIMAP_WIDTH/2, mmY + MINIMAP_HEIGHT/2 };
+      
+      DrawRectangle(mmX - MINIMAP_BORDER, mmY - MINIMAP_BORDER, MINIMAP_WIDTH + MINIMAP_BORDER*2, MINIMAP_HEIGHT + MINIMAP_BORDER*2, WHITE);
+      DrawRectangle(mmX, mmY, MINIMAP_WIDTH, MINIMAP_HEIGHT, BLACK); // Black if out of bounds
+      
+      // Minimap initilization
+      BeginScissorMode(mmX, mmY, MINIMAP_WIDTH, MINIMAP_HEIGHT);
+        
+        BeginMode2D(minimapCam);
+            
+            DrawTexture(background, 0, 0, LIGHTGRAY);
+            
+            for (int i=0; i<MAX_VEHICLES; i++) {
+                if(vehicles[i].type == POLICE)  {
+                    DrawRectangle(vehicles[i].posx, vehicles[i].posy, 40, 60, BLUE);
+                }
+            }
+
+             DrawTexturePro(deliveryBikeRender.texture, bikeSource, destRect, origin, 180, WHITE);
+
+        EndMode2D();
+        
+      EndScissorMode(); 
+      
+      DrawRectangleLines(mmX, mmY, MINIMAP_WIDTH, MINIMAP_HEIGHT, BLACK);
       
       if (IsKeyPressed(KEY_K)) {
             showOrders = !showOrders;
@@ -82,12 +143,12 @@ int main(void) {
       
       if (showOrders)   {
           DrawRectangle (10, 10, 140, 170, WHITE);
-          DrawText("Fuck PAOK", 15, 15 + count*25, 20, BLACK);
+          DrawText("Orders", 15, 15 + count*25, 20, BLACK);
       }
     EndDrawing();
   }
   
-  UnloadTexture (Texture2DBackground);
+  UnloadTexture (background);
   UnloadRenderTexture(deliveryBikeRender);
   CloseWindow();
 
