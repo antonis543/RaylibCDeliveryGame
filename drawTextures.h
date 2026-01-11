900f9dbeee7e4ef26433ea44118bf5006af38d21
Antonis Karafotis <antoniskarafotis2@gmail.com>
2026-01-11 19:05:19 +0200

    Add drawTextures.h header file for texture functions

---
+++ drawTextures.h
@@ -0,0 +1,23 @@
+/*
+ * Πανεπιστήμιο: Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης
+ * Τμήμα: Τμήμα Ηλεκτρολόγων Μηχανικών και Μηχανικών Υπολογιστών
+ * Μάθημα: Δομημένος Προγραμματισμός (004)
+ * Τίτλος Εργασίας: Raylib Food Delivery Game
+ * Συγγραφείς: 
+ * - Αντώνιος Καραφώτης (ΑΕΜ: 11891)
+ * - Νικόλαος Αμοιρίδης (ΑΕΜ: 11836)
+ * Άδεια Χρήσης: MIT License
+ * (Δείτε το αρχείο LICENSE.txt για το πλήρες κείμενο)
+ */
+
+#ifndef DRAWTEXTURES_H
+#define DRAWTEXTURES_H
+
+#include "raylib.h"
+
+void DrawDeliveryBike(RenderTexture2D target);
+void PrepareCarTexture(RenderTexture2D target);
+void PrepareTruckTexture(RenderTexture2D target);
+void PreparePoliceTexture(RenderTexture2D target);
+
+#endif
